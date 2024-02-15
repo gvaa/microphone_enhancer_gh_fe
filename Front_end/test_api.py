cdd1a0c70f16113752081be6cc49027368df917f
Valentin Golobokin <109627508+gvaa@users.noreply.github.com>
2024-02-15 12:42:14 +0100

    Update test_api.py

--- Front_end/test_api.py
+++ Front_end/test_api.py
@@ -1,35 +1,50 @@
-"""
-API test
-"""
-
 import os
 import requests
+from PIL import Image
 import streamlit as st
 
-url = "https://micenhancerapi-3t3dih6maa-oa.a.run.app/" # "https://micenhancerapi-3t3dih6maa-oa.a.run.app/", "http://127.0.0.1:8000/"
+url = "http://127.0.0.1:8000/" # "https://micenhancerapi-3t3dih6maa-oa.a.run.app/", "http://127.0.0.1:8000/"
 upload_url = url + 'upload_file'
 serve_out_url = url + 'audio_out'
+serve_in_url = url + 'audio_in'
 
-'''
-### Test FE for audio enhancers
-'''
+st.subheader('Microphone quality enhancer')
+st.caption('enhance the quality of your microphone')
+st.divider()
+
 enhancer = st.radio("Select enhancer:",
                     ["speechbrain/sepformer-dns4-16k-enhancement",
                      "speechbrain/sepformer-wham16k-enhancement",
-                     "NOT IMPLEMENTED YET: microphone_enhancer_gh"],
+                     "microphone_enhancer_gh/autoencoder_10_256",
+                     "microphone_enhancer_gh/conv_autoencoder_16_32_64_32_16_1"],
                     index=0,
                     )
-st.write(enhancer, " is selected.")
 uploaded_file = st.file_uploader("Choose a noisy audio file (.wav):", type='wav')
+
+st.divider()
+
 if uploaded_file is not None:
-    st.write("Uploaded noisy audio:")
-    st.write(uploaded_file)
-    st.audio(uploaded_file)
+    col1, col2 = st.columns(2, gap='medium')
+    with col1:
+        st.write("Uploaded noisy audio:")
+    with col2:
+        st.write(f"Audio cleaned with {enhancer}:")
+    col3, col4 = st.columns(2, gap='medium')
+    with col3:
+        st.audio(uploaded_file)
     file = {'file': uploaded_file}
     params = {'enhancer': enhancer}
     cleaned = requests.post(upload_url,
                             files=file,
                             data=params).json()
-    st.write(f"Audio cleaned with {enhancer}:")
-    st.write(cleaned)
-    st.audio(os.path.join(serve_out_url, cleaned['cleaned_file_name']), format="audio/wav")
+    with col4:
+        st.audio(os.path.join(serve_out_url, cleaned['cleaned_file_name']), format="audio/wav")
+    col5, col6 = st.columns(2, gap='medium')
+    with col5:
+        with Image.open(requests.get((os.path.join(serve_in_url, cleaned['cleaned_file_name']+'.jpg')), stream=True).raw) as image:
+            st.image(image)
+        #st.image(os.path.join(serve_in_url, cleaned['cleaned_file_name'])+'.jpg')
+    with col6:
+        with Image.open(requests.get((os.path.join(serve_out_url, cleaned['cleaned_file_name']+'.jpg')), stream=True).raw) as image:
+            st.image(image)
+        #st.image(os.path.join(serve_out_url, cleaned['cleaned_file_name'])+'.jpg')
