4c8406e1d02dad31f9b1694b10f429046a95ea14
Valentin Golobokin <109627508+gvaa@users.noreply.github.com>
2024-02-16 13:28:37 +0100

    Update test_api.py

--- Front_end/test_api.py
+++ Front_end/test_api.py
@@ -4,14 +4,22 @@ import requests
 from PIL import Image
 import streamlit as st
 
-url = "https://micenhancerapiretrained-3t3dih6maa-oa.a.run.app/" # "https://micenhancerapi-3t3dih6maa-oa.a.run.app/", "http://127.0.0.1:8000/"
+url = "https://micenhancerapi-3t3dih6maa-oa.a.run.app/" # "https://micenhancerapi-3t3dih6maa-oa.a.run.app/", "http://127.0.0.1:8000/"
 upload_url = url + 'upload_file'
 serve_out_url = url + 'audio_out'
 serve_in_url = url + 'audio_in'
 
-st.subheader('Microphone quality enhancer')
-st.caption('enhance the quality of your microphone')
+col5, col6 = st.columns([10,1], gap='medium')
+with col5:
+    st.subheader('Microphone quality enhancer')
+    st.caption('enhance the quality of your microphone')
+with col6:
+    with Image.open(requests.get('https://raw.githubusercontent.com/gvaa/microphone_enhancer_gh_fe/master/Front_end/microphone-162205_1280.png', stream=True).raw) as micro:
+        st.image(micro, width = 40)
+
 st.divider()
+
+
 
 enhancer = st.radio("Select enhancer:",
                     ["speechbrain/sepformer-dns4-16k-enhancement",
@@ -25,19 +33,21 @@ uploaded_file = st.file_uploader("Choose a noisy audio file (.wav):", type='wav'
 st.divider()
 
 if uploaded_file is not None:
+    file = {'file': uploaded_file}
+    params = {'enhancer': enhancer}
     col1, col2 = st.columns(2, gap='medium')
     with col1:
-        st.write("Uploaded noisy audio:")
-    with col2:
-        st.write(f"Audio cleaned with {enhancer}:")
+        st.markdown(f'<p style="background-color:#fcefec;color:#972007;border-radius:7px;"><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Uploaded noisy audio:<br> <br> </p>', unsafe_allow_html=True) #st.write("Uploaded noisy audio:")
     col3, col4 = st.columns(2, gap='medium')
     with col3:
         st.audio(uploaded_file)
-    file = {'file': uploaded_file}
-    params = {'enhancer': enhancer}
-    cleaned = requests.post(upload_url,
-                            files=file,
-                            data=params).json()
+    with col2:
+        with st.spinner(f"Enhancing with {enhancer.split('/')[1]}:"):
+            cleaned = requests.post(upload_url,
+                                files=file,
+                                data=params).json()
+        st.success(f"Enhanced with {enhancer.split('/')[1]}:")
+        #st.write(f"Audio cleaned with {enhancer}:")
     with col4:
         req = urllib.request.urlopen(os.path.join(serve_out_url, cleaned['cleaned_file_name'])).read()
         st.audio(req)
